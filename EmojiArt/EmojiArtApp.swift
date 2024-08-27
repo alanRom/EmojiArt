a3fbdf35ea8b08334e39a57398d0cfd8dbea6218
Alan Romano <alan.rom25.95@gmail.com>
2024-08-27 17:51:53 -0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Alan Romano on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
