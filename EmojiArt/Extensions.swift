c9e4d83761228e919d331a6f79c76a8ef39fffce
Alan Romano <alan.rom25.95@gmail.com>
2024-08-28 10:23:31 -0400

    Create New App, Set Up Model, ViewModel, and View

--- EmojiArt/Extensions.swift
+++ EmojiArt/Extensions.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+typealias CGOffset = CGSize
+
+extension String {
+    var uniqued: String {
+        reduce(into: "") { sofar, element in
+            if !sofar.contains(element) {
+                sofar.append(element)
+            }
+        }
+    }
+}
+
+extension CGRect {
+    var center: CGPoint {
+        CGPoint(x: midX, y: midY)
+    }
+    init(center: CGPoint, size: CGSize) {
+        self.init(origin: CGPoint(x: center.x-size.width/2, y: center.y-size.height/2), size: size)
+    }
+}
+
+extension CGOffset {
+    static func +(lhs: CGOffset, rhs: CGOffset) -> CGOffset {
+        CGOffset(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+    static func +=(lhs: inout CGOffset, rhs: CGOffset) {
+        lhs = lhs + rhs
+    }
+}
